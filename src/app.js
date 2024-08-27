8f8bbd374378e59bc322f03b23d684cd6076b45a
DAZ <daz4126@gmail.com>
2024-08-27 09:10:22 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -103,5 +103,6 @@ surge({
      }
   },
   clear: $ =>  Array.from($.table.querySelectorAll("input")).forEach(cell => cell.value = ""),
+  share: $ => navigator.share({title:"I cracked the Fruit And Veg Cipher!",text:`My score was ${$.finalScore.value}!`}),
   connect: $ => $._id = Math.round((new Date().setHours(0,0,0,0) - new Date(2024,7,18))/3600000/24)
 })
