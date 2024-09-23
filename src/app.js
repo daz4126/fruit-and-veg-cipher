c0356b523ded1511d2340babfa771c3ca839eac9
DAZ <daz4126@gmail.com>
2024-09-23 22:40:27 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -148,7 +148,7 @@ surge({
   clear: $ =>  Array.from($.table.querySelectorAll("input")).forEach(cell => cell.value = ""),
   share: $ => navigator.share({title:"I cracked the Fruit And Veg Cipher!",text:`I cracked the 🍏Fruit And Veg Cipher🥦!! My score was ${$.finalScore.value}!`}),
   connect: $ => {
-    $._id = localStorage.getItem("fruit&veg-cipher-lastplayed") === (new Date).toLocaleDateString() ? null :Math.round((new Date().setHours(0,0,0,0) - new Date(2024,7,18))/3600000/24)
+    $._id = localStorage.getItem("fruit&veg-cipher-last-played") === (new Date).toLocaleDateString() ? null :Math.round((new Date().setHours(0,0,0,0) - new Date(2024,7,18))/3600000/24)
     if(localStorage.getItem("fruit&veg-cipher-played-already") === "true"){
       startGame($)
     }
